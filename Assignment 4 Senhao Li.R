cd6c3439cc3daac97f5be114f144a74fa575c6a1
robinlish <35699446+robinlish@users.noreply.github.com>
2018-02-28 11:11:46 -0500

    new update

--- Assignment 4 Senhao Li.R
+++ Assignment 4 Senhao Li.R
@@ -1,4 +1,6 @@
 library("tibble")
+library("ggplot2")
+library("tidyverse")
 #10.5 exercise
 #1.How can you tell if an object is a tibble? 
 mtcars
@@ -97,3 +99,83 @@ who5 %>%
   ggplot(aes(x = year, y = cases, group = country_sex, colour = sex)) +
   geom_line()
 who5
+
+#table 4 to table 6
+library(foreign)
+library(stringr)
+library(plyr)
+library(reshape2)
+source("xtable.r")
+
+# Data from http://pewforum.org/Datasets/Dataset-Download.aspx
+
+# Load data -----------------------------------------------------------------
+
+pew <- read.spss("pew.sav")
+pew <- as.data.frame(pew)
+
+
+religion <- pew[c("q16", "reltrad", "income")]
+religion$reltrad <- as.character(religion$reltrad)
+religion$reltrad <- str_replace(religion$reltrad, " Churches", "")
+religion$reltrad <- str_replace(religion$reltrad, " Protestant", " Prot")
+religion$reltrad[religion$q16 == " Atheist (do not believe in God) "] <- "Atheist"
+religion$reltrad[religion$q16 == " Agnostic (not sure if there is a God) "] <- "Agnostic"
+religion$reltrad <- str_trim(religion$reltrad)
+religion$reltrad <- str_replace_all(religion$reltrad, " \\(.*?\\)", "")
+
+religion$income <- c("Less than $10,000" = "<$10k", 
+                     "10 to under $20,000" = "$10-20k", 
+                     "20 to under $30,000" = "$20-30k", 
+                     "30 to under $40,000" = "$30-40k", 
+                     "40 to under $50,000" = "$40-50k", 
+                     "50 to under $75,000" = "$50-75k",
+                     "75 to under $100,000" = "$75-100k", 
+                     "100 to under $150,000" = "$100-150k", 
+                     "$150,000 or more" = ">150k", 
+                     "Don't know/Refused (VOL)" = "Don't know/refused")[religion$income]
+
+religion$income <- factor(religion$income, levels = c("<$10k", "$10-20k", "$20-30k", "$30-40k", "$40-50k", "$50-75k", 
+                                                      "$75-100k", "$100-150k", ">150k", "Don't know/refused"))
+
+counts <- count(religion, c("reltrad", "income"))
+names(counts)[1] <- "religion"
+head(counts)
+xtable(counts[1:10, ], file = "pew-clean.tex")
+
+# Convert into the form in which I originally saw it -------------------------
+
+raw <- dcast(counts, religion ~ income)
+xtable(raw[1:10, 1:7], file = "pew-raw.tex")
+head(raw)
+table6 <- gather(raw, income,freq,"<$10k", "$10-20k", "$20-30k", "$30-40k", "$40-50k", "$50-75k", "$75-100k", "$100-150k",">150k", "Don't know/refused")
+table6 <- arrange(table6,religion)
+head(table6)
+
+#table 7 to table 8
+options(stringsAsFactors = FALSE)
+library(lubridate)
+library(reshape2)
+library(stringr)
+library(plyr)
+source("xtable.r")
+
+raw <- read.csv("billboard.csv")
+raw <- raw[, c("year", "artist.inverted", "track", "time", "date.entered", "x1st.week", "x2nd.week", "x3rd.week", "x4th.week", "x5th.week", "x6th.week", "x7th.week", "x8th.week", "x9th.week", "x10th.week", "x11th.week", "x12th.week", "x13th.week", "x14th.week", "x15th.week", "x16th.week", "x17th.week", "x18th.week", "x19th.week", "x20th.week", "x21st.week", "x22nd.week", "x23rd.week", "x24th.week", "x25th.week", "x26th.week", "x27th.week", "x28th.week", "x29th.week", "x30th.week", "x31st.week", "x32nd.week", "x33rd.week", "x34th.week", "x35th.week", "x36th.week", "x37th.week", "x38th.week", "x39th.week", "x40th.week", "x41st.week", "x42nd.week", "x43rd.week", "x44th.week", "x45th.week", "x46th.week", "x47th.week", "x48th.week", "x49th.week", "x50th.week", "x51st.week", "x52nd.week", "x53rd.week", "x54th.week", "x55th.week", "x56th.week", "x57th.week", "x58th.week", "x59th.week", "x60th.week", "x61st.week", "x62nd.week", "x63rd.week", "x64th.week", "x65th.week", "x66th.week", "x67th.week", "x68th.week", "x69th.week", "x70th.week", "x71st.week", "x72nd.week", "x73rd.week", "x74th.week", "x75th.week", "x76th.week")]
+names(raw)[2] <- "artist"
+
+raw$artist <- iconv(raw$artist, "MAC", "ASCII//translit")
+raw$track <- str_replace(raw$track, " \\(.*?\\)", "")
+names(raw)[-(1:5)] <- str_c("wk", 1:76)
+raw <- arrange(raw, year, artist, track)
+
+long_name <- nchar(raw$track) > 20
+raw$track[long_name] <- paste0(substr(raw$track[long_name], 0, 20), "...")
+
+xtable(raw[c(1:3, 6:10), 1:8], "billboard-raw.tex")
+
+table8 <- gather(raw,key="week", value="rank",str_c("wk", 1:76)) 
+table8 <- arrange(table8,artist) %>% 
+  na.omit(table8)
+table8$week <- str_replace(table8$week,"wk","")
+head(table8)
